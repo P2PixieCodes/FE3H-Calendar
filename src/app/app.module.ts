983d3636e9308d5bcbafee212519d609dfaa3d3f
P2PixieCodes <meer.schneid@gmail.com>
2023-08-16 11:17:38 +0200

    MOD: provide value for APP_BASE_HREF in module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { CommonModule } from '@angular/common';
+import { APP_BASE_HREF, CommonModule } from '@angular/common';
 import { ScaleToFractionDirective } from './scale-to-fraction.directive';
 import { ScaleBackgroundToFitDirective } from './scale-background-to-fit.directive';
 
@@ -18,7 +18,9 @@ import { ScaleBackgroundToFitDirective } from './scale-background-to-fit.directi
     CommonModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [
+    {provide: APP_BASE_HREF, useValue: '/fe3h-calendar'}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
